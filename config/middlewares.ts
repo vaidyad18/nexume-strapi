3e01cf1b193ae256c2f8ccaf8a87ca9527e8a958
Vaidya Dandriyal <vaidyadandriyal04@gmail.com>
2025-08-11 02:20:10 +0530

    solved cors issue

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,17 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['https://nexume-ai-resume-builder.vercel.app'], // your frontend URL
+      headers: '*',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
